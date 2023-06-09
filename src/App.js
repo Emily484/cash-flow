f6960a7ae8ce025f75f903793f1e5fb239005df3
Emily484 <63012821+Emily484@users.noreply.github.com>
2023-06-09 11:40:01 -0700

    setting up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/Login';
+import { Auth } from './pages/auth';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <h1>My Budget App</h1>
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
